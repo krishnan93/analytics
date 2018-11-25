4ff4324f627654bc7f861700552ee33a2b0518f4
krishnan93 <pg17abhinav_n@mdi.ac.in>
2018-11-25 13:56:24 +0530

    regression and logarithmic on 25th nov

---
+++ logarithmic_titanic.R
@@ -0,0 +1,62 @@
+logdata<- read.csv("titanic_varun.csv", header =TRUE )
+(logdata)
+summary(logdata)
+boxplot(logdata)
+hist(logdata$age)
+logdata$age[is.na(logdata$age)]=29
+hist(logdata$age)
+summary(logdata)
+structure(logdata)
+read(logdata)
+logdata$female=ifelse(logdata$sex=="female",1,0)
+logdata$portc=ifelse(logdata$embarked=="c",1,0)
+logdata$ports=ifelse(logdata$embarked=="s",1,0)
+(logdata)
+summary(logdata)
+logdata[,c(3,4,9),]
+data2<-logdata[,-c(3,4,9),]
+(data2)
+box=boxplot(data2$age)
+box$stats
+ifelse(data2$age<4,4,data$age)
+data3<-logdata[,-c(3,4,9),]
+data3
+data3$age=ifelse(data3$age<4,4,data3$age)
+(boxplot(data3$age))
+(box)
+box2=boxplot(data3$age)
+data3$age=ifelse(data3$age>52,52,data3$age)
+(boxplot(data3$age))
+box4=boxplot(data3$fare)
+box4$stats
+quantile(data3$fare,seq(0,1,0.01))
+data3$fare=ifelse(data3$fare>136,136,data3$fare)
+box4=boxplot(data3$fare)
+box4$stats
+?set.seed
+set.seed(123)
+index=sample(x=1:nrow(data3), size=0.7* nrow(data3))
+train=data3[index,]
+test=data3[-index,]
+library(car)
+mod=lm(survived~.,train)
+summary(mod)
+t=vif(mod)
+?vif
+model1=glm(as.factor(survived)~.,family="binomial",train)
+str(data3$survived)
+class(data3$survived)
+(model1)
+summary(model1)
+stepmod= step(model1, direction = "both")
+bestmodel=glm(as.factor(survived) ~ pclass + age + sibsp + female, family="binomial",train)
+summary(bestmodel)
+train$score= predict(bestmodel,newdata=train, type="response")
+(train$score)
+(train)
+library(lattice)
+library(ggplot2)
+library(caret)
+library(e1071)
+library(Matrix)
+install.packages("lattice")
--- practice from internet regression.R
+++ practice from internet regression.R
@@ -7,3 +7,43 @@ summary(fit2)
 predict(fit2, newdata=data.frame(speed=c(5,8)))
 plot(fit2)
 
+# Create Training and Test data -
+set.seed(100)  # setting seed to reproduce results of random sampling
+trainingRowIndex <- sample(1:nrow(cars), 0.8*nrow(cars))  # row indices for training data
+trainingData <- cars[trainingRowIndex, ]  # model training data
+testData  <- cars[-trainingRowIndex, ]   # test data
+
+# Build the model on training data -
+lmMod <- lm(dist ~ speed, data=trainingData)  # build the model
+distPred <- predict(lmMod, testData)  # predict distance
+
+summary (lmMod)  # model summary
+#> 
+#> Call:
+#> lm(formula = dist ~ speed, data = trainingData)
+#> 
+#> Residuals:
+#>     Min      1Q  Median      3Q     Max 
+#> -23.350 -10.771  -2.137   9.255  42.231 
+#> 
+#> Coefficients:
+#>             Estimate Std. Error t value Pr(>|t|)    
+#> (Intercept)  -22.657      7.999  -2.833  0.00735 ** 
+#> speed          4.316      0.487   8.863 8.73e-11 ***
+#> ---
+#> Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+#> 
+#> Residual standard error: 15.84 on 38 degrees of freedom
+#> Multiple R-squared:  0.674,  Adjusted R-squared:  0.6654 
+#> F-statistic: 78.56 on 1 and 38 DF,  p-value: 8.734e-11
+AIC (lmMod)  # Calculate akaike information criterion
+#> [1] 338.4489
+
+actuals_preds <- data.frame(cbind(actuals=testData$dist, predicteds=distPred))  # make actuals_predicteds dataframe.
+correlation_accuracy <- cor(actuals_preds)  # 82.7%
+head(actuals_preds)
+
+(min_max_accuracy <- mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max)))
+# => 58.42%, min_max accuracy
+(mape <- mean(abs((actuals_preds$predicteds - actuals_preds$actuals))/actuals_preds$actuals) ) 
+# => 48.38%, mean absolute percentage deviation
